9d23c1fa4ed34df7115ff3c40a6bfc984cef6d49
Gustavo-Morchel <gustavomorchel@gmail.com>
2025-06-05 21:50:06 -0300

    feat: start first React project
    Descri: Set up the initial structure to begin learning React fundamentals such as components and JSX.

---
+++ PirmeiroReact/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+
+
+function App() {
+  return <div>
+    <TitlteColorido/>
+    <TitlteColorido/>
+  </div>
+  
+}
+
+function TitlteColorido(){
+  return <h1 className="titulo-colorido">Título Colorido</h1>
+}
+
+
+export default App
