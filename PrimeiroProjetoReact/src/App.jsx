ef093e8491c9cc7fad0ecf147c576b1a1bcb9a20
Gustavo-Morchel <gustavomorchel@gmail.com>
2025-06-06 02:03:54 -0300

    test: setup Node.js + React + Vite environment

---
+++ PrimeiroProjetoReact/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return <div>
+    <TittleColor/>
+  </div>
+}
+
+function TittleColor(){
+  return <h1 className="title-color">Teste Titulo</h1>
+}
+
+export default App
