6ef1e7c8ae03936d17e25cda346cd8735dfb134a
Gustavo-Morchel <gustavomorchel@gmail.com>
2025-06-12 21:41:51 -0300

    Testes de Responsividade
    Teste de responsividade usando react

--- PrimeiroProjetoReact/src/App.jsx
+++ PrimeiroProjetoReact/src/App.jsx
@@ -7,7 +7,21 @@ function App() {
 }
 
 function TittleColor(){
-  return <h1 className="title-color">Teste Titulo</h1>
+  return (
+    // <div className="container">
+    //   <div className="linha">
+    //     <img src="https://images.unsplash.com/photo-1493612276216-ee3925520721?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="quadrado" alt="" />
+    //     <img src="https://images.unsplash.com/photo-1493612276216-ee3925520721?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="quadrado black" alt="" />
+    //   </div>
+    //   <div className="linha">
+    //     <img src="https://images.unsplash.com/photo-1493612276216-ee3925520721?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="quadrado" alt="" />
+    //     <img src="https://images.unsplash.com/photo-1493612276216-ee3925520721?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="quadrado azul" alt="" />
+    //   </div>
+    // </div>
+    <div className="navbar">
+      
+    </div>
+  );
 }
 
 export default App
